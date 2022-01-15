7e0d2d587cc594293809d812cf184c6cb51acd77
pablop442 <pablo442@gmail.com>
2022-01-15 16:46:30 +0000

    added icons

--- src/front/js/component/Cards/Cards.jsx
+++ src/front/js/component/Cards/Cards.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./cards.css"
+import{FaCity, FaBusAlt, FaEnvelopeOpenText, FaFileAlt, FaIdBadge, FaPersonBooth, FaStore, FaUsersCog} from "react-icons/fa"
+import{GiTeamIdea} from "react-icons/gi"
+const iconStyles = {
+    color: "#023047",
+    display: "inline-block",
+    margin: "5px",
+    fontSize: "35px"
+};
 
 export const Cards = () => {
 	return (
@@ -8,9 +16,10 @@ export const Cards = () => {
       <div className="container">
         <div className="row mt-2">
           <div className="col-sm-4">
-            <div class="card">
+            <div class="card p-4">
               <div className="card-body">
-                <h5 className="card-title">Proposals</h5>
+                
+                <h5 className="card-title"><GiTeamIdea style={iconStyles}/> Proposals</h5>
                 <p className="card-text">
                 We are glad to hear and resolve any proposal or claim that you may have. 
                 </p>
@@ -18,9 +27,9 @@ export const Cards = () => {
             </div>
           </div>
           <div className="col-sm-4">
-            <div class="card">
+            <div class="card p-4">
               <div className="card-body">
-                <h5 className="card-title">Projects</h5>
+                <h5 className="card-title"> <FaCity style={iconStyles}/> Projects</h5>
                 <p className="card-text">
                 Keep updated of all the offcial projects that are being developed in your city.
                 </p>
@@ -28,9 +37,9 @@ export const Cards = () => {
             </div>
           </div>
           <div className="col-sm-4">
-            <div class="card">
+            <div class="card p-4">
               <div className="card-body">
-                <h5 className="card-title">Public Voting</h5>
+                <h5 className="card-title"> <FaPersonBooth style={iconStyles}/> Public Voting</h5>
                 <p className="card-text">
                 You are your city’s voice. Participate in the matters that are being put to vote.
                 </p>
@@ -38,11 +47,11 @@ export const Cards = () => {
             </div>
           </div>
         </div>
-        <div className="row mt-2">
+        <div className="row mt-4">
           <div className="col-sm-4">
-            <div class="card">
+            <div class="card p-4">
               <div className="card-body">
-                <h5 className="card-title">Services</h5>
+                <h5 className="card-title"> <FaBusAlt style={iconStyles}/> Services</h5>
                 <p className="card-text">
                 Transportation schedules and routes, city bike, park information and more.
                 </p>
@@ -50,9 +59,9 @@ export const Cards = () => {
             </div>
           </div>
           <div className="col-sm-4">
-            <div class="card">
+            <div class="card p-4">
               <div className="card-body">
-                <h5 className="card-title">Citizen File</h5>
+                <h5 className="card-title"> <FaIdBadge style={iconStyles}/> Citizen File</h5>
                 <p className="card-text">
                 Update and follow up the status of  your proposal or claim. 
                 </p>
@@ -60,9 +69,9 @@ export const Cards = () => {
             </div>
           </div>
           <div className="col-sm-4">
-            <div class="card">
+            <div class="card p-4">
               <div className="card-body">
-                <h5 className="card-title">City News</h5>
+                <h5 className="card-title"><FaStore style={iconStyles}/> City News</h5>
                 <p className="card-text">
                 Follow most recent and all the daily events of your city.
                 </p>
