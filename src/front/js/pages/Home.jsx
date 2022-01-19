2425be083ee6171c2676b69f261dc96ccaa73b6c
CRodr86 <rodrigocfotografia@gmail.com>
2022-01-19 15:47:26 +0000

    Added TrendingNews component and designed the basic structure

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import TrendingNews from "../component/TrendingNews/TrendingNews.jsx";
 
 const Home = () => {
   const { store, actions } = useContext(Context);
 
-  return <div></div>;
+  return (
+    <>
+      <TrendingNews />
+    </>
+  );
 };
 export default Home;
