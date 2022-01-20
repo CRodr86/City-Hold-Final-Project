e948047ea6354df2f797e657e9d9ee91941e3c54
CRodr86 <rodrigocfotografia@gmail.com>
2022-01-20 11:30:02 +0000

    changed logo size

--- src/front/js/component/Navbar/Navbar.jsx
+++ src/front/js/component/Navbar/Navbar.jsx
@@ -10,20 +10,22 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-white">
         <div className="container">
-          <a className="navbar-brand" href="#">
-            <Logo width="68px" height="80px" viewBox="0 0 2.2 2.58" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <div>
+            <a className="navbar-brand" href="#">
+              <Logo width="44.88px" height="52.8px" viewBox="0 0 2.2 2.58" />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <NavbarItem
