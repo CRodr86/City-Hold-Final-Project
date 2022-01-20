9c341d8055ff906718bb8c6b1c400aa8ac478f43
pablop442 <pablo442@gmail.com>
2022-01-20 12:16:42 +0000

    added hover to navbar

--- src/front/js/component/NavbarItem/NavbarItem.jsx
+++ src/front/js/component/NavbarItem/NavbarItem.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./navbarItem.css";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const NavbarItem = (props) => {
   return (
     <>
-      <li className={props.liClassName}>
-        <a
+      <li className={props.liClassName} id="navitem">
+        <Link
           className={props.aClassName}
           aria-current={props.ariaCurrent}
           to={props.to}
         >
           {props.pageName}
-        </a>
+        </Link>
       </li>
     </>
   );
