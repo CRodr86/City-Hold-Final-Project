ff3cf29f2015eacf358274c0cd7e3ec3e4e586fb
CRodr86 <rodrigocfotografia@gmail.com>
2022-01-21 14:46:36 +0000

    added progress bars and divided in smaller components

---
+++ src/front/js/component/NewsCard/NewsCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const NewsCard = (props) => {
+  return (
+    <div className="card border-0 me-4 smallCard rounded-0">
+      <img
+        src={props.imgSrc}
+        className="card-img-top rounded-0"
+        alt={props.imgAlt}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props.cardTitle}</h5>
+        <p className="card-text">{props.cardText}</p>
+      </div>
+    </div>
+  );
+};
+
+NewsCard.propTypes = {
+  imgSrc: PropTypes.string,
+  imgAlt: PropTypes.string,
+  cardTitle: PropTypes.string,
+  cardText: PropTypes.string,
+};
+
+export default NewsCard;
