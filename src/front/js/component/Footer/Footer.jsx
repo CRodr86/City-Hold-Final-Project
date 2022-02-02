9fa5c743c648b4ea4d9feb240730969b6ed6d1f9
CRodr86 <rodrigocfotografia@gmail.com>
2022-02-02 12:46:49 +0000

    made footer responsive

--- src/front/js/component/Footer/Footer.jsx
+++ src/front/js/component/Footer/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = () => {
   return (
     <footer className=" pt-5 pb-3 border-top footer">
       <div className="container">
-        <div className="row row-cols-5">
+        <div className="row row-cols-lg-5 row-cols-md-2 row-cols-2">
           <div className="col">
             <a href="/" className="mb-3 link-dark text-decoration-none">
               <Logo width="34px" height="40px" viewBox="0 0 2.2 2.58" />
             </a>
             <p className="text mt-2">Keep the city connected</p>
           </div>
-          <div className="col"></div>
-          <div className="col">
+          <div className="col d-none d-lg-block"></div>
+          <div className="col d-none d-lg-block">
             <h5 className="footerLabel">Pages</h5>
             <ul className="nav flex-column">
               <FooterLink to="/" pageName="Home" />
@@ -25,7 +25,7 @@ const Footer = () => {
               <FooterLink to="/publicvoting" pageName="Public Voting" />
             </ul>
           </div>
-          <div className="col">
+          <div className="col d-none d-lg-block">
             <h5 className="footerLabel">Sign in</h5>
             <ul className="nav flex-column">
               <FooterLink to="myfile" pageName="My Account" />
@@ -45,11 +45,11 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        <div className="row row-cols-2 mt-2">
-          <div className="col">
+        <div className="row row-cols-lg-2 row-cols-md-1 row-cols-1 mt-2">
+          <div className="col d-flex justify-content-lg-start justify-content-md-center justify-content-center">
             <p>Copyright © 2021 Town-Hold Team</p>
           </div>
-          <div className="col d-flex justify-content-end">
+          <div className="col d-flex justify-content-lg-end justify-content-md-center justify-content-center">
             <a href="#" className="d-inline nav-link p-0 text-muted">
               Privacy Policy
             </a>
--- src/front/js/component/FooterIcons/FooterIcons.jsx
+++ src/front/js/component/FooterIcons/FooterIcons.jsx
@@ -6,8 +6,9 @@ import { SiEnvato } from "react-icons/si";
 const FooterIcons = () => {
   return (
     <li className="nav-item mb-2">
-      <ImFacebook2 height={40} /> <ImInstagram /> <ImTwitter />{" "}
-      <AiFillDribbbleCircle /> <SiEnvato />
+      <ImFacebook2 className="me-2" /> <ImInstagram className="me-2" />{" "}
+      <ImTwitter className="me-2" /> <AiFillDribbbleCircle className="me-2" />{" "}
+      <SiEnvato />
     </li>
   );
 };
