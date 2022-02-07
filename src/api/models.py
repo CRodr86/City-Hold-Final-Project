c80592ea4562281349e324bc3d224db3b7a72f95
pablop442 <pablo442@gmail.com>
2022-02-07 19:14:22 +0000

    added models

--- src/api/models.py
+++ src/api/models.py
@@ -1,23 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+# from sqlachemy import Column, ForeignKey, Integer, String
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'user'
+    # __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
     lastname = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     home_phone = db.Column(db.String(20), unique=False, nullable=True)
     mobile_phone = db.Column(db.String(20), unique=True, nullable=False)
-    adress = db.Column(db.String(240), unique=False, nullable=False)
+    address = db.Column(db.String(240), unique=False, nullable=False)
     zip_code = db.Column(db.String(20), unique=False, nullable=False)
     years_of_residence = db.Column(db.String(20), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
@@ -27,9 +28,9 @@ class User(db.Model):
             "email": self.email,
             "home phone": self.home_phone,
             "mobile phone": self.mobile_phone,
-            "adress": self.adress,
+            "address": self.address,
             "zip code": self.zip_code,
-            "years of residence": self.years_of_residence;
+            "years of residence": self.years_of_residence,
             # do not serialize the password, its a security breach
         }
 
@@ -37,10 +38,11 @@ class User(db.Model):
 class Proposal(db.Model):
     __tablename__ = 'proposal'
     id = db.Column(db.Integer, primary_key=True)
-    proponent = Column(Integer, ForeignKey('user.id'))
+    # proponent = db.Column(db.String, db.ForeignKey(User.name))
+    # user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     area = db.Column(db.String(20), unique=False, nullable=False)
     proposal_type = db.Column(db.String(20), unique=False, nullable=False)
-    date = db.Column(db.Date, unique=False, nullable=False)
+    date = db.Column(db.String, unique=False, nullable=False)
     description = db.Column(db.String(1000), unique=False, nullable=False)
     documents = db.Column(db.String(120), unique=False, nullable=False)
     document_type = db.Column(db.String(20), unique=False, nullable=False)
@@ -48,13 +50,14 @@ class Proposal(db.Model):
     contact_by = db.Column(db.String(20), unique=False, nullable=False)
     confirmation_by = db.Column(db.String(20), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<Proposal %r>' % self.proposal_id
+    # def __repr__(self):
+    #     return '<Proposal %r>' % self.proponent
 
     def serialize(self):
         return {
             "id": self.id,
-            "proponent": self.proponent,
+            # "proponent": self.proponent,
+            # "user_id" : self.user_id,
             "area": self.area,
             "proposal type": self.proposal_type,
             "date": self.date,
@@ -63,6 +66,6 @@ class Proposal(db.Model):
             "document type": self.document_type,
             "document description": self.document_description,
             "contact by": self.contact_by,
-            "confirmation by": self.confirmation_by;
+            "confirmation by": self.confirmation_by,
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,8 +2,9 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Proposal
 from api.utils import generate_sitemap, APIException
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,36 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/user', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_users), 200
+
+@api.route('/user/<int:id>', methods=['GET'])
+def get_one_user(id):
+    user_x = User.query.get(id)
+    if user_x is None:
+        return 'User not found', 404
+    else:
+        return jsonify(user_x.serialize()), 200
+
+@api.route('/user', methods=['POST'])
+def create_users():
+    request_body = request.get_json()
+    new_user = User(name= request_body["name"], lastname= request_body["lastname"], email= request_body["email"], home_phone= request_body["home phone"], mobile_phone= request_body["mobile phone"], address= request_body["address"], zip_code= request_body["zip_code"], years_of_residence= request_body["years_of_residence"], password= request_body["password"], is_active= request_body["is_active"])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(request_body), 201
+
+@api.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user_x = User.query.get(id)
+    if user_x is None:
+        return 'User not found', 404
+    else:
+        db.session.delete(user_x)
+        db.session.commit()
+        return jsonify("User deleted"), 200
