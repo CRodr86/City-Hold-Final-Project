0d98db9028455f378999e6f3706513dc6dd7ab7d
CRodr86 <rodrigocfotografia@gmail.com>
2022-03-22 18:28:35 +0000

    changed favicon logo

--- webpack.common.js
+++ webpack.common.js
@@ -46,7 +46,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: "favicon.ico",
+      favicon: "city-hold.ico",
       template: "template.html",
     }),
     new Dotenv({ safe: true, systemvars: true }),
